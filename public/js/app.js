6ee3785434a630a4685dce79c0f190809f185dda
jefzainjan <jefzainjan@gmail.com>
2016-12-09 16:43:09 +0800

    Come

--- public/js/app.js
+++ public/js/app.js
@@ -6,56 +6,56 @@ class App{
 
   {
         "id": 1,
-        "name": "Ford Everest",
-        "details": "Manual, Diesel, ₱ 1,339,000",
+        "name": "Ford Explorer",
+        "details": "Explorer Platinum grabs attention with a distinctive grille with satin-chrome finish, dual integrated chrome exhaust tips and 20-inch bright-machined wheels with dark-painted Pockets. The interior is just as impressive with the standard dual-panel moonroof, unique Nirvana leather with micro-perforation and quilted inserts. SYNC® 3 is our easy-to-use, responsive voice-activated technology. Discover the system’s quick reactions to your conversational voice commands. Experience the smartphone-like touchscreen with impressive responsiveness. The available 2.3L EcoBoost® engine produces 280 hp68 and provides 310 lb.-ft. of torque.68 It has a twin-scroll low-inertia turbocharger that separates the exhaust pulses from pairs of cylinders, enabling quicker turbine spin-up, enhanced acceleration responsiveness and efficiency. <br> Manual, Diesel, ₱ 1,339,000</br>",
         "image": "img/Ford.jpg",
       
       },
       {
         "id": 2,
         "name": "BMW Gran-Turismo 335i",
-        "details": "Manual, Gasoline, PRICE ON REQUEST  ",
+        "details": "The distinct, aggressive look is clear from every angle. Its coupe-inspired aerodynamic design is complemented by Air Breathers and an active rear spoiler that reduces lift on the rear axle by 35 percent. Featuring a fully electric slide and lift control, it also includes a wind deflector to keep the wind out and an electric sunshade to keep the cabin cool on even the hottest days. Trunk capacity is 24.6 cubic feet, transforming to 56.5 cubic feet when the rear seat is folded down. A power tailgate is standard while the Comfort Access Function, included in the 340i, opens and closes the tailgate with a wave of your foot under the bumper. The 3 Series Gran Turismo’s versatile 8-speed Sport transmission makes for a smooth ride at any speed. 330i: BMW's mighty inline 4-cylinder TwinPower Turbo. Displacing just 2.0 liters, this 248-hp marvel generates 258 lb-ft of torque at just 1450 rpm, for potent thrust that kicks in right from the start and continues all the way up to 4800 rpm. <br>Manual, Gasoline, PRICE ON REQUEST</br>  ",
         "image": "img/bmw.jpg",
        
       },
       {
         "id": 3,
-        "name": "Chevrolet Trailblazer 2016",
-        "details": "Automatic Diesel ₱ 1,509,888",
+        "name": "Chevrolet Trailblazer",
+        "details": "The New 2017 Chevrolet Trailblazer boasts of an improved interior and exterior and reduced cabin and engine noise The New 2017 Chevrolet Trailblazer comes with advanced safety features including Lane Departure Warning, Forward Collision Alert and Side Blind Zone Alert. Fitted with the Chevrolet’s latest MyLink Infotainment System with multiple smartphone OS compatibility and Siri Eyes Free. Available in three variants: 2.8L Z71 AT, 2.8L LT AT, 2.5L LT MT<br>Automatic Diesel ₱ 1,509,888</br>",
         "image": "img/Chevy.jpg",
       },
       {
         "id": 4,
         "name": "FOTON View Traveller",
-        "details": "Manual Diesel ₱ 1,299,000 ",
+        "details": "The Traveller is a van. A very modern, very large van, but still a van. Like many Chinese vans, it shares a lot of similarities with the Toyota HiAce, styling-wise. While the bubble roof grants it bus-like interior space, it does give it a rather odd appearance, even compared to the tall-roofed HiAces plying our streets. The 2.8-liter Cummins turbodiesel makes just 130hp, but short gearing and an aggressive boost profile make it very lively. There's more than enough oomph to overtake, and while the claimed top speed is a paltry 145kph, that's more due to the van's brick-like aerodynamics than a lack of power. With a 2.3-ton curb weight, a tall driving position and long-geared steering, piloting the View is much like driving a bus.<br>Manual Diesel ₱ 1,299,000</br> ",
         "image": "img/Foton.jpg",
        
       },
       {
         "id": 5,
         "name": "Honda City",
-        "details": "CVT, Gasoline, ₱ 804,000",
+        "details": "The 1.5-liter i-Vtec of The All-New Honda City can make short work of any urban challenge. Variable valve timing and lift electronic control technology optimizes engine response based on the driver’s instantaneous demands, thus providing spirited dynamics while maintaining fuel economy. <br>CVT, Gasoline, ₱ 804,000<br>",
         "image": "img/Honda.jpg",
       },
       {
 
         "id": 6,
         "name": "Hyundai Santa Fe",
-        "details": "Manual, Diesel, ₱ 1,568,000",
+        "details": "Santa Fe provides an ideal 360 degree camera view via clear display with visual information including the blind spot all around the vehicle in diverse roads and driving conditions. Along with 8 color TFT smart navigation system for diverse driving information and multi-media, discover true-to-life sounds of radio, CD or MP3 files with the premium system that comes in standard or alternative options with 6 speakers. Santa Fe is equipped with front side airbags, driver and passenger airbags as well as front and rear curtain airbags to minimize passenger injuries in the event of collisions. <br>Manual, Diesel, ₱ 1,568,000</br>",
         "image": "img/Hyundai.jpg",
       },
       {
 
         "id": 7,
         "name": "Isuzu D-Max",
-        "details": "Manual, Diesel, ₱ 1,190,000",
+        "details":"The new D-Max has a notable rear bed size at 1,485 mm x 1,530 mm x 465 mm, which can carry a multitude of large and heavy items. It can also be optioned with an array of accessories like bed covers, camper shells, and more. it has automatic climate control and a 7-inch LCD multimedia head unit with DVD/WMA/MP3 compatibility and Bluetooth/iPod connectivity for the comfort and entertainment of everyone inside. Inside its bonnet is a reliable 3.0L 4JJI-TC (HI) diesel power plant with VGS turbocharger and intercooler, coupled to either a 5-speed manual transmission or a 5-Speed automatic transmission with sequential shift. Performance output reaches 163 hp and 380 Nm of torque – both available in 4x2 and 4x4 variants.<br>Manual, Diesel, ₱ 1,190,000</br>",
         "image": "img/Izuzu.jpg",
       },
        {
 
         "id": 8,
         "name": "Jeep Grand Cherokee",
-        "details": "Automatic, Gasoline, ₱ 3,860,000",
+        "details": "The Jeep Grand Cherokee is a luxury vehicle in addition to being a solid highway cruiser and a capable off-road machine.The current-generation Grand Cherokee was introduced for the 2011 model year. For 2016, Jeep Grand Cherokee is available in a new High Altitude edition based on the top Overland trim.Electric power steering is new for 2016, and the 2016 Grand Cherokee SRT gets a revised instrument cluster. The powertrain lineup ranges from a fuel-efficient EcoDiesel V6 to gasoline V6 and V8 engines, topped by the outlandishly quick Grand Cherokee SRT. Three engines are available for the Grand Cherokee, four if the SRT is counted.Base models use a 3.6-liter V6, which makes 295 horsepower and is fitted with fuel-saving engine stop-start technology.The 5.7-liter Hemi V8 generates 360 horsepower.The most fuel-efficient option is the EcoDiesel 3.0-liter V6, producing 420 pound-feet of torque.Estimated at 30 mpg on the highway, it stretches range per tankful to 730 miles. Jeep also offers a fourth powertrain, in the performance-focused SRT, whose 6.4-liter Hemi V8 now whips up 475 horsepower.All that energy reaches all four wheels, whereas other Grand Cherokees can have either rear-wheel drive or four-wheel drive.<br>Automatic, Gasoline, ₱ 3,860,000</br>",
         "image": "img/Jeep.jpg",
       },
        
@@ -74,26 +74,26 @@ class App{
     component.innerHTML = html;
   }
 
-carDesign(){
-    let id = document.getElementById('car1');
+carDesign1(){
+    
+    
     let name = document.getElementById('car2');
     let details = document.getElementById('car3');
-    let image = document.getElementById('car4');
+    let image = document.getElementById('car5');
   
 
     let car = {      
-      "id": id.value,
+    
       "name": name.value,
       "details": details.value,
       "image": image.value,
-     
-    };
+      };
 
-
+    name.value = details.value = image.value = '';
     this.car.push(car);
 
     
-    id.value = name.value = details.value = image.value = ''; 
+    
   } 
 
   carDelete(key){
@@ -107,6 +107,25 @@ carDesign(){
     this.carList();
   }
 
+  updateCar(id){
+
+    let qwe = {
+      "id" :  id,
+      "name" : $('#asd').val(),
+      "description" : $('#zxc').val()
+    }
+
+    let r = this.car;
+    for(let i=0;i<r.length;i++){
+      if(r[i].id == id){
+        r[i] = qwe;
+        break;
+      }
+    }
+
+    this.carList();
+
+}
   SearchCarById(id){
     let r = this.car;
     for(let i=0;i<r.length;i++){
@@ -115,6 +134,8 @@ carDesign(){
       }
     }
   } 
+
+ 
 
   SearchCar(name){
     let objects = [];
@@ -139,27 +160,30 @@ class Component extends App{
 carDesignLayout(){
   let html = `
 
+<ul id="dropdown1" class="dropdown-content #827717 lime darken-4">
+  <li><a href="#" onclick="component.latestCar()">Sell</li>
+</ul>
 <nav>
-    <div class="nav-wrapper  #26a69a teal lighten-1">
-      <a href="#" onclick="component.carList()" class="brand-logo"> &nbsp&nbsp&nbsp&nbspCariTown </a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li>
+  <div class="nav-wrapper #827717 lime darken-4">
+  <img src="img/ccc.png" width="50px">
+    <a href="#!" onclick="component.Home()" class="brand-logo">CariTown</a>
+    <ul class="right hide-on-med-and-down">
       <li><a href="#" onclick="component.Home()">Home</a></li>
-    <li><a href="#" onclick="component.carList()">Cars</a></li>  
-  <li><a href="#" onclick="component.latestCar()">Sale a Car</a></li>
-  
-      </ul>
+      <li><a href="#" onclick="component.carList()">Find your Car</a></li>
+      
+      <!-- Dropdown Trigger -->
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Sell<i class="material-icons right">arrow_drop_down</i></a></li>
+    </ul>
+  </div>
+</nav>
 
-     
-    </div>
-    </nav>
-
-<div class="slider">
+<div class="slider" >
     <ul class="slides">
       <li>
-        <img src="img/2.jpg"> <!-- random image -->
+        <img src="img/1.jpg"> <!-- random image -->
         <div class="caption center-align">
-          <h3>Our Best Seller of the month</h3>   
+        <h5>CariTown! Make your choice.</h5>
+          <h3>Buy and sell cars!</h3>   
         </div>
       </li>
       <li>
@@ -169,7 +193,7 @@ carDesignLayout(){
         </div>
       </li>
       <li>
-        <img src="img/1.jpg"> <!-- random image -->
+        <img src="img/2.jpg"> <!-- random image -->
         <div class="caption right-align">
           <h5 class="light grey-text text-lighten-3">Makes you feel comfortable</h5>
         </div>
@@ -177,15 +201,12 @@ carDesignLayout(){
       <li>
         <img src="img/4.jpg"> <!-- random image -->
         <div class="caption center-align">
-          <h3>Buy and sell cars!</h3>
+          <h3>Best Seller of the month</h3>
           
         </div>
       </li>
     </ul>
   </div>
-
-  
-
 
 </div>
 
@@ -197,18 +218,19 @@ carDesignLayout(){
         <div id="latestCar"></div>
         
   
-        <footer class="page-footer #26a69a teal lighten-1">
+        <footer class="page-footer #9e9d24 lime darken-3">
           <div class="container">
             <div class="row">
               <div class="col l6 ">
-                <h5 class="white-text">CariTown</h5>
-                <p class="grey-text text-lighten-4">CariTown can easily sell your car or buy a car in just a simple click.</p>
+                <h2 class="white-text">CariTown</h2>
+                <p class="grey-text text-lighten-4">CariTown can easily sell your car in just a simple click.</p>
               </div>
             </div>
           </div>
-          <div class="footer-copyright #009688 teal">
+          <div class="footer-copyright #827717 lime darken-4">
             <div class="container">
-            © 2016 Copyright Text
+            <img src="img/ccc.png" width="50px">
+            © 2016 Copyright Text/CariTown
             </div>
           </div>
         </footer>
@@ -229,26 +251,25 @@ Car(id){
 
     let html = `
 
-     <h5 class="center-align">${r.name}</h5>
-      <div class="row">       
-        <div class="col s12 m12">
-          <div class="cardpanel teal">
-            <div class="card-image">
-              <img src="${r.image}">
-            </div>
-            <div class="card-stacked">
-              <div class="card-content">
-                <p>${r.details}</p>
-              </div>
-            <div class="card-action small">               
-                <span onclick="component.carDelete(${r.id})" class="new badge small red" data-badge-caption="">Delete Base</span>
-               <span onclick="component.carList()" class="new badge small" data-badge-caption="">Back to List</span>
-              </div>            
-            </div>     
-          </div>   
-          
-        </div>      
+    <div class="col s12 m7">
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="${r.image}">
       </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>${r.details}</p>
+        </div>
+        <div class="card-action">
+          <span onclick="component.carDelete(${r.id})" class="new badge small" data-badge-caption="">Remove</span>
+               <span onclick="component.carList()" class="new badge small" data-badge-caption="">Back</span>
+               <span onclick="component.updateCar()" class="new badge small" data-badge-caption="">Update</span>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+
     `;
 
     this.reRender(`   
@@ -258,19 +279,18 @@ Car(id){
     $('#carRecent').hide();
     $('#carList').hide();
     $('#latestCar').hide();
-
-      $('#Home').hide();
+    $('#buyList').show();
+    $('#Home').hide();
   }
+
 
   carList(){
     let html = `
 
 
-
-
       <br/>
         <nav>
-          <div class="nav-wrapper white">
+          <div class="nav-wrapper #9e9d24 lime darken-3">
           <form>
             <div class="input-field">       
               <input onkeyup="component.carItems(this.value)" id="search" type="search" placeholder="Search" required>
@@ -287,6 +307,7 @@ Car(id){
     html += `
       <div class="row" id="carItems">
     `;
+
     let r = this.car;
     for(let i=0;i<r.length;i++){
       html+= `
@@ -315,8 +336,9 @@ Car(id){
     $('#carList').show();
     $('#Car').hide();
     $('#carRecent').hide();
-    $('#latestCar').hide();    
-         $('#Home').hide();
+    $('#latestCar').hide();  
+    $('#buyList').show();  
+    $('#Home').hide();
   }
 
 carItems(name){
@@ -348,7 +370,33 @@ carItems(name){
     $('#Car').hide();
     $('#carRecent').hide();  
     $('#latestCar').hide();
-        $('#Home').hide();    
+    $('#buyList').show();
+    $('#Home').hide();    
+  }
+
+  updateExFeeling(id){
+    
+    let html = `
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="asd" type="text" class="validate" value="${this.car[id].name}">          
+        </div>
+        <div class="input-field col s6">
+          <input id="zxc" type="text" class="validate" value="${this.car[id].details}">
+        </div>
+      </div>
+      <span onclick="component.buyList()" class="new badge small" data-badge-caption="">Update</span>
+      <hr/>
+    `;  
+    this.reRender(`
+      ${html}
+      `,document.getElementById("carItems"));
+    $('#carList').show();
+    $('#Car').hide();
+    $('#carRecent').hide();  
+    $('#latestCar').hide();
+    $('#buyList').show();
+    $('#Home').hide();    
   }
 
   carRecent(){
@@ -365,7 +413,7 @@ carItems(name){
     for(let i=(r.length-1);i>=0;i--){
       if(count++ === 6)break;
       html+= `
-        <div class="col s12 m4">
+     <div class="col s12 m4">
           <div class="card small hoverable">
             <div class="card-image">
               <img src="${r[i].image}">
@@ -379,6 +427,7 @@ carItems(name){
             </div>
           </div>
         </div>
+
       `;
     }
 
@@ -396,8 +445,9 @@ carItems(name){
      
           
        <div class="row">
+
             <div class="input-field col s6 ">
-           
+            <h3>Your Car</h3>
               <input disabled value="${this.car.length+1}" id="car" type="text"class=" validate" >
             </div>
               <div class="row">
@@ -421,8 +471,8 @@ carItems(name){
        
         <div align="left">
        
-       <button onclick="component.carList()" class="btn waves-effect waves-light">Back</button>
-       <button onclick="component.carDesign()" class="btn waves-effect waves-light">Save</button>
+       <button onclick="component.carList()" class="btn #ffa726 orange lighten-1">Back</button>
+       <button onclick="component.carDesign1()" class="btn #ffa726 orange lighten-1">Sell</button> 
          </div>
         
       
@@ -436,8 +486,14 @@ carItems(name){
     $('#carList').hide();
     $('#Car').hide();
     $('#carRecent').hide(); 
+    $('#buyList').show();
     $('#Home').hide();    
   }
+
+  // buyList(){
+
+  // }
+     
 
   Home(){
   let html=`
@@ -451,7 +507,8 @@ carItems(name){
     $('#latestCar').hide();
     $('#carList').hide();
     $('#Car').hide();
-    $('#carRecent').show();  
+    $('#carRecent').show(); 
+    $('#buyList').show(); 
     $('#Home').hide();  
 
 }
